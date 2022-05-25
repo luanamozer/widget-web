3c48d1bc17465480ce67abcb58e3c46964ae17a9
Luana Mozer <luanamozzer1996@gmail.com>
2022-05-24 22:19:23 -0300

    config inicial do envio de email mailtrap

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7b4acbcbf1165e",
+    pass: "52fe7b6b5d1e8c",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Luana Mozzer <luanamozzer1996@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size:16px; color: "#111">`,
+      `<p>Tipo de feedback: ${type}</p>`,
+      `<p>Comentário do feedback: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });

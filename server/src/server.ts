612a401d401197469d9dd80da081b8ea0a2d8b32
Luana Mozer <luanamozzer1996@gmail.com>
2022-06-13 14:18:46 -0300

    add port env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import process from "process";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log("HTTP ronding");
+app.listen(process.env.PORT || 3333, () => {
+  console.log("HTTP server ronding");
 });
